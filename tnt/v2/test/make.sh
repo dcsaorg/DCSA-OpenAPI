60effae4bd6df3e544ef338f2835bdcba1fe9269
Henrik Helmø Larsen <helmoe@gmail.com>
2022-05-30 13:59:55 +0200

    Update make.sh

--- tnt/v2/test/make.sh
+++ tnt/v2/test/make.sh
@@ -1,3 +1,3 @@
-npx redoc-cli bundle ebl.yaml
+npx redoc-cli bundle tnt_v2.2.0.yaml
 mv redoc-static.html index.html
 sed -i '7 i \ \ <link rel="icon" type="image/png" href="./favicon-32x32.png" sizes="32x32"/>' index.html
