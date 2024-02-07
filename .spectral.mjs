fa82ab73a854444e687989379f7c235d0c9dc137
henrik.larsen@dcsa.org <henrik.larsen@dcsa.org>
2024-02-07 10:26:50 +0100

    Modified 6 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyMDE3MQ/spectral.js?branch_slug=ArrivalNotice&token=5b111a64-f0fb-4df4-9d2d-fd7863737dd9";
+export default { extends: ruleset };
